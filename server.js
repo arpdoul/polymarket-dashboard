ea6ec54eacb1c605d8b6630495be678a1432c9bd
Abdulsamad Umar Ladan <arpdoul@gmail.com>
2026-04-18 00:04:36 +0100

    use dynamic port for Render deployment

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ app.get('/api/markets', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Running at http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Running at port ' + PORT));
